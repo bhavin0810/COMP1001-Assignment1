27e6687ad826270306dfa536032de62615663b16
bhavin0810 <bhavin0810@gmail.com>
2015-01-30 20:04:06 -0500

    Creating Tester Class with main method

---
+++ src/Tester.java
@@ -0,0 +1,14 @@
+
+/**
+ * @author: Bhavin Patel
+ * @Description: This Class Tester to test the program
+ * @version 1.0  Jan-30-2015 Creating Tester Class with main method 
+ */
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
